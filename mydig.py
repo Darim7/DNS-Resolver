7a61205a0bf6d886aa4a3acf7e7dd9094c7bef5e
Zhenting Ling <zhenting.ling@stonybrook.edu>
2025-02-09 00:23:30 -0500

    Implemented query function.

--- mydig.py
+++ mydig.py
@@ -1,4 +1,6 @@
-import dns
+import dns.exception
+import dns.message
+import dns.query
 import sys
 
 # Root servers from iana.org
@@ -9,5 +11,21 @@ root_servers = [
     "192.58.128.30", "193.0.14.129", "199.7.83.42", "202.12.27.33"
 ]
 
+def query(name: str, type: str, servers: list[str]):
+    q = dns.message.make_query(name, type)
+
+    response = None
+    for server in servers:
+        try:
+            response = dns.query.udp(q, server, timeout=5)
+            break
+        except dns.exception.Timeout:
+            continue
+
+    return response
+
 if __name__ == "__main__":
-    print(f"You just asked: {sys.argv}")
+    name, record_type = sys.argv[1], sys.argv[2]
+    print(f"You just asked: {name} IN {record_type}")
+    res = query(name, record_type, root_servers)
+    print(f"You got an answer: {res.to_text()}")
